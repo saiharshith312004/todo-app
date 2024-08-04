8b677b5416c4ff8a3aa0e628c0df36f5edcda45f
sai harshith parlapalli <149226897+saiharshith312004@users.noreply.github.com>
2024-08-05 02:01:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ def get_todos():
 
 @app.route('/todos', methods=['POST'])
 def add_todo():
-    todo = request.json
+    todo = request.json.get('todo')
     todos.append(todo)
-    return jsonify(todo), 201
+    return jsonify({'todo':todo}), 201
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
